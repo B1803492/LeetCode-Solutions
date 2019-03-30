6d7b69f45cef82e642f3695e69486bb45890968e
B1803492 <B1803492@users.noreply.github.com>
2019-03-30 16:17:09 +0800

    1

--- Solution5/DY_5.cpp
+++ Solution5/DY_5.cpp
@@ -1,3 +1,7 @@
+/*Runtime: 4 ms, faster than 100.00% of C++ online
+submissions for Longest Palindromic Substring.
+Memory Usage: 8.5 MB, less than 100.00% of C++ online
+submissions for Longest Palindromic Substring.*/
 class Solution {
 public:
     string longestPalindrome(string s) {
